0f9ab732909a9674809f122d4222259e9b2fabed
Marcos <sombromar@gmail.com>
2020-12-09 19:39:41 -0300

    player2 apreto start

---
+++ Calcu/javaPlayer2.js
@@ -0,0 +1,23 @@
+var valorB1;
+
+var boton1 = document.getElementsByName("botones");
+
+//boton1.E.addEventListener("click", botonRead);
+
+//var xd = setInterval(botonRead, 100);
+
+//var botonRead = () => {console.log("xd");}}
+
+/*function botonRead(valor)
+{
+    valorB1 = valor.target.value;
+    console.log("funciona");
+}
+*/
+
+boton1.forEach(function(boton){
+    boton.addEventListener("click", function(){
+        valorB1 = boton.value;
+        console.log(valorB1);
+    })
+})
