a9974000cf5ce1a955754dbc1e87bcbced6a4124
Marcos <sombromar@gmail.com>
2020-12-09 23:41:16 -0300

    representacion en el display

--- Calcu/javaPlayer2.js
+++ Calcu/javaPlayer2.js
@@ -1,10 +1,41 @@
-var valorB1;
+var valorNum;
+var operator;
+var numText = "";
+var num1;
+var cuenta;
+var valorDisplay = "";
 
-var boton1 = document.getElementsByName("botones");
+var numbers = document.getElementsByName("numbers_html");
+var logicOps = document.getElementsByName("logicOperators_html");
+var display1 = document.getElementById("display_html");
 
-boton1.forEach(function(boton){
-    boton.addEventListener("click", function(){
-        valorB1 = boton.innerText;
-        console.log(valorB1);
+numbers.forEach(function(value){
+    value.addEventListener("click", function(){
+        valorNum = value.innerText;
+        operation();
     })
 })
+
+logicOps.forEach(function(value){
+    value.addEventListener("click", function(){
+        operator = value.innerText;
+        operator = " " + operator + " ";
+        operation2();
+    })
+})
+
+function operation()
+{
+    numText += valorNum;
+    num1 = parseFloat(numText);
+    valorDisplay += valorNum;
+    display1.value = valorDisplay;
+    console.log(valorDisplay);
+}
+
+function operation2()
+{
+    valorDisplay += operator;  
+    display1.value = valorDisplay;
+    console.log(valorDisplay);
+}
