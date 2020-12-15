0609f3afd1aaba865646f4ddcc61068ac9c71575
Marcos <sombromar@gmail.com>
2020-12-15 19:50:01 -0300

    ta

--- Calcu/javaPlayer2.js
+++ Calcu/javaPlayer2.js
@@ -1,12 +1,19 @@
 var valorNum;
 var operator;
 var numText = "";
-var num1;
+var numReal;
 var cuenta;
 var valorDisplay = "";
+var resultado = "";
+var valorActual = "";
+var cuentaActual = "";
+var opMulti = false;
+
+var operacion = "";
 
 var numbers = document.getElementsByName("numbers_html");
 var logicOps = document.getElementsByName("logicOperators_html");
+var buttons = document.getElementsByName("buttons_html");
 var display1 = document.getElementById("display_html");
 
 numbers.forEach(function(value){
@@ -24,18 +31,67 @@ logicOps.forEach(function(value){
     })
 })
 
-function operation()
-{
+buttons.forEach(function(value){
+    value.addEventListener("click", function(){
+        if (value.innerText == "E"){
+            result();
+        }else{
+            display1.value = "Ready";
+            valorDisplay = "";
+            numText = "";
+            numReal = 0;
+            resultado = "";
+            valorActual = "";
+            cuentaActual = "";
+            resultado = "";
+            opMulti = false;
+            operacion = "";
+        }
+    })
+})
+
+function operation(){
     numText += valorNum;
-    num1 = parseFloat(numText);
+    numReal = parseFloat(numText);
     valorDisplay += valorNum;
     display1.value = valorDisplay;
-    console.log(valorDisplay);
 }
 
-function operation2()
-{
+function operation2(){
     valorDisplay += operator;  
     display1.value = valorDisplay;
-    console.log(valorDisplay);
+}
+
+function result(){
+    for (dato of valorDisplay){
+        if (dato == "×"){
+            operacion += "*";
+        }else{
+            operacion += dato;
+        }
+    }
+    display1.value = eval(operacion);
+
+    /*for (letra of valorDisplay){
+        if (letra == "+"){
+            cuentaActual += valorActual + "+";
+            valorActual = "";
+            continue;
+        }
+        if (letra == "×"){
+            opMulti = true;
+            continue;
+        }
+        if (letra == " "){
+            continue;        
+        }else if (opMulti == false){
+            valorActual += letra;
+        }else{
+            opMulti = false;
+            cuentaActual += parseFloat(valorActual) * parseFloat(letra);
+            valorActual = "";
+        }
+    }
+    console.log(cuentaActual, valorActual, resultado);
+    */
 }
