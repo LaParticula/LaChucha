2d56b743a11a6cccfe604c5ab19bfb200e207881
Marcos <sombromar@gmail.com>
2020-12-17 21:30:05 -0300

    animacion con una img

--- VillaM/granja.js
+++ VillaM/granja.js
@@ -1,24 +1,28 @@
 var canvas = document.getElementById("villa");
 var ctx = canvas.getContext("2d");
-canvas.width = 500;
-canvas.height = 500;
+
+var canWidth = canvas.width;
+var canHeight = canvas.height;
+var canX = 100;
+var canY = 100;
+var imgPosX = 0;
+var imgPosY = 0;
+var imgWidth = 200;
+var imgHeight = 167;
 
 var cowImage = new Image();
-
-cowImage.src = `tenor-0.png`;
-cowImage.onload = function(){ctx.drawImage(cowImage, 100, 100);}; 
+cowImage.src = `vacaRePiolaBailando.png`;
+cowImage.onload = () => {setInterval(fps, 100);}; 
 
 var numberOfImages = 20;
 var currentImage = 0;
 
 function fps(){
-    currentImage = ++currentImage % numberOfImages;
-    cowImage.src = `tenor-${currentImage}.png`;
-    
     ctx.clearRect(0, 0, 500, 500);
-    ctx.drawImage(cowImage, 100, 100);
-
+    ctx.drawImage(cowImage, imgPosX, imgPosY, imgWidth, imgHeight, canX, canY, imgWidth, imgHeight);
     console.log(currentImage);
-}
+    console.log(imgPosX);
+    currentImage = ++currentImage % numberOfImages;
+    imgPosX = currentImage * imgWidth;
 
-setInterval(fps, 100);
+}
